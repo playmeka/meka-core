df6550712640ef222e33f5ca4e3589f4020f8606
Paul Fletcher-Hill <pfletcherhill@gmail.com>
2020-01-08 15:54:30 -0800

    Export modules

---
+++ src/index.js
@@ -0,0 +1,6 @@
+export { default, Action } from "./Battle";
+export { default as Team } from "./Team";
+export { default as HQ } from "./HQ";
+export { default as ObjectWithPosition, Position } from "./ObjectWithPosition";
+export { default as Citizen } from "./Citizen";
+export { default as Fighter } from "./Fighter";
